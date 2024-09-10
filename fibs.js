aa07dd9174ccedb695c7b4037f774d717b00aade
Jyjar <156258612+Jyjar@users.noreply.github.com>
2024-09-10 23:02:28 +0200

    Done with the fibonacci sequence part.

---
+++ fibs.js
@@ -0,0 +1,20 @@
+function fibs(n) {
+    let array = [];
+    for (i = 0; i < n; i++) {
+        if (i <= 1) {
+            array.push(i);
+        } else {
+            array.push(array[i - 1] + array[i - 2]);
+        }
+    }
+    return array;
+}
+
+function fibsRec(n, fibs = [0, 1]) {
+    if (n <= fibs.length) {
+        return fibs.slice(0, n);
+    } else {
+        fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+        return fibsRec(n, fibs);
+    }
+}
